7a1882ab7b8e021ae1894eca693ce471b85488ad
yukund <yanbo.deng@gmail.com>
2013-05-06 22:53:25 +1200

    updated overload methods

--- src/main/java/group/scjp/guide/chapter2/OverloadBook.java
+++ src/main/java/group/scjp/guide/chapter2/OverloadBook.java
@@ -17,14 +17,17 @@ public class OverloadBook extends Book{
 	public Integer showIndex(Integer i) throws Exception { return i;}
 
 	public void showPage(){PrintUtil.Print("original showPage");};
-	
 	// a method can be overloaed in the same class
-	public void showPage(int pageNumber){PrintUtil.Print("overloading showPage with PageNumber: " +pageNumber);}
+	public void showPage(Long pageNumber){PrintUtil.Print("my input parameter type is Long");}
+	protected void showPage(int pageNumber){PrintUtil.Print("my input parameter type is int");}
+	// in the same class, the overload cannot be overloaded by changing return type only
+	//private String showPage(int pageNumber){PrintUtil.Print("my return type is string"); return "";}
 	
-	public static void main(String[] strings){
+	public static void main(String[] str){
 		OverloadBook overloadBook = new OverloadBook();
 		overloadBook.showPage();
 		overloadBook.showPage(1);
+		overloadBook.showPage(1L);
 		
 	}
 }
